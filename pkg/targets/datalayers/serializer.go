718783036e18eb63695fe421126c7b8c87db9bda
Shicong <niebayes@gmail.com>
2024-07-12 17:03:12 +0800

    fix: add tests for serializer and fix serialization

--- pkg/targets/datalayers/serializer.go
+++ pkg/targets/datalayers/serializer.go
@@ -7,6 +7,9 @@ import (
 	"github.com/timescale/tsbs/pkg/data/serialize"
 )
 
+// The placeholder of a nil value in the serialized data point.
+var NULL string = "nil"
+
 // A serializer that implements the PointSerializer interface and is used
 // by Datalayers to serialize simulated data points during data generation.
 type Serializer struct{}
@@ -19,6 +22,14 @@ type Serializer struct{}
 // Currently, the TSBS benchmark suite only supports bool, i32, i64, f32, f64, binary, string types.
 // As a consequence, the serializer for Datalayers only supports serializing those types.
 func (s *Serializer) Serialize(p *data.Point, w io.Writer) error {
+	numTags := len(p.TagKeys())
+	numFields := len(p.FieldKeys())
+
+	// Rejects this data point if it does not contain any valid key values.
+	if numTags+numFields == 0 {
+		return nil
+	}
+
 	buf := make([]byte, 0, 256)
 
 	// Appends the measurement name.
@@ -30,37 +41,37 @@ func (s *Serializer) Serialize(p *data.Point, w io.Writer) error {
 	buf = append(buf, ' ')
 
 	// Appends tags.
-	appended_tags, buf := appendKeyValues(p.TagKeys(), p.TagValues(), buf)
-	buf = append(buf, ' ')
+	buf = appendKeyValues(p.TagKeys(), p.TagValues(), buf)
+	if numTags > 0 && numFields > 0 {
+		buf = append(buf, ' ')
+	}
 
 	// Appends fields.
-	appended_fields, buf := appendKeyValues(p.FieldKeys(), p.FieldValues(), buf)
+	buf = appendKeyValues(p.FieldKeys(), p.FieldValues(), buf)
 	buf = append(buf, '\n')
-
-	// Rejects this data point if it does not contain any valid key values.
-	if appended_tags+appended_fields == 0 {
-		return nil
-	}
 
 	// Writes the serialized data point.
 	_, err := w.Write(buf)
 	return err
 }
 
-// Appends all key-value pairs into the buffer.
-// Returns how many key-value pairs are appended and the extended buffer.
-func appendKeyValues(keys [][]byte, values []interface{}, buf []byte) (int, []byte) {
+// Appends all key-value pairs into the buffer and returns the extended buffer.
+func appendKeyValues(keys [][]byte, values []interface{}, buf []byte) []byte {
 	for i := 0; i < len(keys); i++ {
 		k := keys[i]
 		v := values[i]
 
 		buf = serialize.FastFormatAppend(k, buf)
 		buf = append(buf, '=')
-		buf = serialize.FastFormatAppend(v, buf)
+		if v != nil {
+			buf = serialize.FastFormatAppend(v, buf)
+		} else {
+			buf = append(buf, NULL...)
+		}
 
 		if i < len(keys)-1 {
 			buf = append(buf, ' ')
 		}
 	}
-	return len(keys), buf
+	return buf
 }
--- pkg/targets/datalayers/serializer_test.go
+++ pkg/targets/datalayers/serializer_test.go
@@ -1,41 +1,58 @@
 package datalayers
 
-
 import (
-	"github.com/timescale/tsbs/pkg/data/serialize"
 	"testing"
+
+	"github.com/timescale/tsbs/pkg/data/serialize"
 )
 
-// TODO(niebayes): remove the old tests and implement our own.
+// Tests that the Datalayers' serializer works as expected.
 func TestDatalayersSerializerSerialize(t *testing.T) {
 	cases := []serialize.SerializeCase{
 		{
+			Desc:       "a Point with no tags and no fields",
+			InputPoint: serialize.TestPointEmpty(),
+			Output:     "",
+		},
+		{
 			Desc:       "a regular Point",
 			InputPoint: serialize.TestPointDefault(),
-			Output:     "cpu,hostname=host_0,region=eu-west-1,datacenter=eu-west-1b usage_guest_nice=38.24311829 1451606400000000000\n",
+			Output:     "cpu 1451606400000000000 hostname=host_0 region=eu-west-1 datacenter=eu-west-1b usage_guest_nice=38.24311829\n",
 		},
 		{
 			Desc:       "a regular Point using int as value",
 			InputPoint: serialize.TestPointInt(),
-			Output:     "cpu,hostname=host_0,region=eu-west-1,datacenter=eu-west-1b usage_guest=38i 1451606400000000000\n",
+			Output:     "cpu 1451606400000000000 hostname=host_0 region=eu-west-1 datacenter=eu-west-1b usage_guest=38\n",
 		},
 		{
 			Desc:       "a regular Point with multiple fields",
 			InputPoint: serialize.TestPointMultiField(),
-			Output:     "cpu,hostname=host_0,region=eu-west-1,datacenter=eu-west-1b big_usage_guest=5000000000i,usage_guest=38i,usage_guest_nice=38.24311829 1451606400000000000\n",
+			Output:     "cpu 1451606400000000000 hostname=host_0 region=eu-west-1 datacenter=eu-west-1b big_usage_guest=5000000000 usage_guest=38 usage_guest_nice=38.24311829\n",
 		},
 		{
 			Desc:       "a Point with no tags",
 			InputPoint: serialize.TestPointNoTags(),
-			Output:     "cpu usage_guest_nice=38.24311829 1451606400000000000\n",
-		}, {
+			Output:     "cpu 1451606400000000000 usage_guest_nice=38.24311829\n",
+		},
+		{
+			Desc:       "a Point with no fields",
+			InputPoint: serialize.TestPointNoFields(),
+			Output:     "cpu 1451606400000000000 hostname=host_0\n",
+		},
+		{
 			Desc:       "a Point with a nil tag",
 			InputPoint: serialize.TestPointWithNilTag(),
-			Output:     "cpu usage_guest_nice=38.24311829 1451606400000000000\n",
-		}, {
+			Output:     "cpu 1451606400000000000 hostname=nil usage_guest_nice=38.24311829\n",
+		},
+		{
 			Desc:       "a Point with a nil field",
 			InputPoint: serialize.TestPointWithNilField(),
-			Output:     "cpu usage_guest_nice=38.24311829 1451606400000000000\n",
+			Output:     "cpu 1451606400000000000 big_usage_guest=nil usage_guest_nice=38.24311829\n",
+		},
+		{
+			Desc:       "a Point with a nil tag and a nil field",
+			InputPoint: serialize.TestPointWithNilTagAndNilField(),
+			Output:     "cpu 1451606400000000000 hostname=nil big_usage_guest=nil\n",
 		},
 	}
 
