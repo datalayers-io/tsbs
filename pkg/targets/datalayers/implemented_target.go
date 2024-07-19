2d75b9c0b015743004b3646a6cdbca3b6de95cfc
Shicong Nie <niebayes@gmail.com>
2024-07-19 16:57:53 +0800

    feat: add partition related configs

--- pkg/targets/datalayers/implemented_target.go
+++ pkg/targets/datalayers/implemented_target.go
@@ -18,6 +18,8 @@ func NewTarget() targets.ImplementedTarget {
 func (t *datalayersTarget) TargetSpecificFlags(flagPrefix string, flagSet *pflag.FlagSet) {
 	flagSet.String(flagPrefix+"sql-endpoint", "127.0.0.1:8360", "Datalayers' Arrow FlightSql endpoint")
 	flagSet.Uint(flagPrefix+"batch-size", 5000, "The capacity of each batch. Should be identical with the runner's batch size configuraiton")
+	flagSet.Uint(flagPrefix+"partition-num", 16, "The number of partitions for each table")
+	flagSet.StringArray(flagPrefix+"partition-by-fields", []string{}, "The partition by fields for each table")
 }
 
 func (t *datalayersTarget) TargetName() string {
