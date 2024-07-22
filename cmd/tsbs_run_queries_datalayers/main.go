23602fdaa143a3a7061205ef727e56fa9d200ab4
Shicong Nie <niebayes@gmail.com>
2024-07-22 16:12:21 +0800

    chore: add skeleton for query runner

--- cmd/tsbs_run_queries_datalayers/main.go
+++ cmd/tsbs_run_queries_datalayers/main.go
@@ -1,1 +1,50 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/blagojts/viper"
+	"github.com/spf13/pflag"
+	"github.com/timescale/tsbs/internal/utils"
+	"github.com/timescale/tsbs/pkg/query"
+)
+
+var (
+	// The Arrow Flight SQL endpoint exposed by the Datalayers server.
+	sqlEndpoint string
+	// The runner for running query benchmarks.
+	runner *query.BenchmarkRunner
+)
+
+func addDatalayersSpecificFlags() {
+	pflag.String("sql-endpoint", "127.0.0.1:8360", "The Arrow Flight SQL endpoint exposed by the Datalayers server")
+}
+
+func init() {
+	// Parse command line args and setup configurations.
+	var config query.BenchmarkRunnerConfig
+	config.AddToFlagSet(pflag.CommandLine)
+	addDatalayersSpecificFlags()
+	pflag.Parse()
+
+	err := utils.SetupConfigFile()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %s", err))
+	}
+	if err = viper.Unmarshal(&config); err != nil {
+		panic(fmt.Errorf("unable to decode config: %s", err))
+	}
+
+	// Set the `sqlEndpoint` global variable.
+	sqlEndpoint = viper.GetString("sql-endpoint")
+	if len(sqlEndpoint) == 0 {
+		panic("missing sql endpoint")
+	}
+
+	// Initialize the runner.
+	runner = query.NewBenchmarkRunner(config)
+}
+
+func main() {
+	runner.Run(&query.FlightSqlQueryPool, newProcessor)
+}
---
+++ pkg/query/datalayers.go
@@ -0,0 +1,70 @@
+package query
+
+import (
+	"fmt"
+	"sync"
+)
+
+// A encoded Arrow Flight SQL query with some metadata attached.
+type FlightSqlQuery struct {
+	HumanLabel       []byte
+	HumanDescription []byte
+	RawQuery         []byte
+	StartTimestamp   int64
+	EndTimestamp     int64
+	id               uint64
+}
+
+// A pool for saving and retriving encoded Arrow Flight SQL queries.
+var FlightSqlQueryPool = sync.Pool{
+	New: func() interface{} {
+		return &FlightSqlQuery{
+			HumanLabel:       []byte{},
+			HumanDescription: []byte{},
+			RawQuery:         []byte{},
+			StartTimestamp:   0,
+			EndTimestamp:     0,
+		}
+	},
+}
+
+// NewFlightSqlQuery returns a new FlightSqlQuery type Query
+func NewFlightSqlQuery() *FlightSqlQuery {
+	return FlightSqlQueryPool.Get().(*FlightSqlQuery)
+}
+
+// GetID returns the ID of this Query
+func (q *FlightSqlQuery) GetID() uint64 {
+	return q.id
+}
+
+// SetID sets the ID for this Query
+func (q *FlightSqlQuery) SetID(n uint64) {
+	q.id = n
+}
+
+// String produces a debug-ready description of a Query.
+func (q *FlightSqlQuery) String() string {
+	return fmt.Sprintf("HumanLabel: \"%s\", HumanDescription: \"%s\"", q.HumanLabel, q.HumanDescription)
+}
+
+// HumanLabelName returns the human readable name of this Query
+func (q *FlightSqlQuery) HumanLabelName() []byte {
+	return q.HumanLabel
+}
+
+// HumanDescriptionName returns the human readable description of this Query
+func (q *FlightSqlQuery) HumanDescriptionName() []byte {
+	return q.HumanDescription
+}
+
+// Release resets and returns this Query to its pool
+func (q *FlightSqlQuery) Release() {
+	q.HumanLabel = q.HumanLabel[:0]
+	q.HumanDescription = q.HumanDescription[:0]
+	q.id = 0
+	q.StartTimestamp = 0
+	q.EndTimestamp = 0
+
+	FlightSqlQueryPool.Put(q)
+}
